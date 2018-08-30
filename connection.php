4ea3711aac5185a462b3a0eee07361968b47471f
Charli <badcoderspaik@gmail.com>
2018-08-30 23:09:15 +0600

    30.08.18

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-//коннект к базе
+//коннект к базе данных
 require_once("php/classes/DbConnector.php");
 $connector = new DbConnector("localhost", "u996357382_learn", "spaik87055091802", "u996357382_learn");
 ?>
